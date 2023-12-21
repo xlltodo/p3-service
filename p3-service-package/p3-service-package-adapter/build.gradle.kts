69d3875aa9eaa50f9ea34aeda944d74c657b3717
lltodo <i@lltodo.com>
2023-12-21 21:13:18 +0800

    -

--- p3-service-package/p3-service-package-adapter/build.gradle.kts
+++ p3-service-package/p3-service-package-adapter/build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
-    id("java")
-}
-
-group = "p3.service"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+    id("org.springframework.boot")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
 
-tasks.test {
-    useJUnitPlatform()
+//    项目模块依赖
+    implementation(":p3-service-package-application")
+
+//    基础框架依赖
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+//    工具依赖
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    compileOnly("com.github.xiaoymin:knife4j-openapi3-jakarta-spring-boot-starter")
+
+//    测试依赖
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
---
+++ p3-service-package/p3-service-package-adapter/src/main/java/com/p3/service/packages/adapter/P3ServicePackageAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.p3.service.packages.adapter;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3ServicePackageAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P3ServicePackageAdapterApplication.class, args);
+    }
+}
