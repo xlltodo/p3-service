33c8ea22e7373c05a0784991c4f99f78ac6eb386
lltodo <i@lltodo.com>
2024-01-03 00:20:12 +0800

    -

---
+++ p3-service-package/p3-service-package-domain/src/main/java/com/p3/service/packages/domain/model/entity/PackageServiceTypeEnum.java
@@ -0,0 +1,11 @@
+package com.p3.service.packages.domain.model.entity;
+
+public enum PackageServiceTypeEnum {
+
+    ATTRIBUTE_SORTING_UNPACKING,  // 分属性拆包
+    REPACKAGING,                  // 重新包装
+    UNBOXING_COUNT,               // 开箱清点数量
+    PRODUCT_PHOTOGRAPHY,          // 产品拍照
+    UNBOXING_QUALITY_CHECK,       // 开箱核对质量
+    EXPRESS_FEE_ADVANCEMENT;      // 代付快递费
+}
